b3396b77281d63589715d2aa285589acf5e5b839
Michael Mroz <mroz15@hotmail.com>
2022-01-13 23:05:56 -0500

    3.5 complete, added POST route

--- index.js
+++ index.js
@@ -50,6 +50,24 @@ app.delete('/api/persons/:id', (request, response) => {
 
 })
 
+app.post('/api/persons', (request, response) => {
+    const data = request.body; 
+    if (!data.name || !data.number) {
+        return response.status(400).json({
+            error: "Entry is missing a name and/or a number."
+        })
+    }
+  else {
+      const person = {
+          id: Math.floor(Math.random() * (10000000 - 1) + 1),
+          name: data.name,
+          number: data.number
+      }
+      persons = persons.concat(person)
+      response.json(person)
+  }
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
