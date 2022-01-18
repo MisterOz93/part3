b6330b06d48c7f33fc2699a7b41a5e6930ca6711
Michael Mroz <mroz15@hotmail.com>
2022-01-17 23:59:23 -0500

    3.9 complete, connected front and backend using cors, added Procfile for next step

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('tiny', {
     skip: (req, res) => {return req.method === 'POST'}
 })); 
+app.use(cors())
 
 let persons = [
     {
