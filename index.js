2b877cf96e2f94d613b9414081345024c80d4741
Michael Mroz <mroz15@hotmail.com>
2022-01-19 23:46:43 -0500

    frontend produciton build created and copied to backend, added static to backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors')
-
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny', {
     skip: (req, res) => {return req.method === 'POST'}
